2046d1a7ff4d3368737a396ea2d5614aa2ff1c98
Shashwat Bhardwaj <shashwatbhardwaj3@gmail.com>
2024-04-17 16:59:50 +0530

    header component done

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header(){
+    return (
+        <div className="head">
+            <img src="public/images/earth.icon.png" alt="earth-icon" className="header-logo"/>
+            
+                <p>My Journal Travel</p>
+        </div>
+    )
+}
