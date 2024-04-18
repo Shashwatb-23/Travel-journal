303e364d3bb517cf9a7e6812224af1b8738a0268
Shashwat Bhardwaj <shashwatbhardwaj3@gmail.com>
2024-04-18 15:07:08 +0530

    added all the descriptions in the data component

--- src/components/Data.js
+++ src/components/Data.js
@@ -11,64 +11,62 @@ export default [
         name:"NEW YORK",
         img:"new-york.jpeg",
         location:"https://maps.app.goo.gl/6yunkVspWXMSuxsq6",
-        description:"New York City is the most diverse and populated city in the United States (the city has a population density of over 27,000 people per square mile). In New York, you’ll find almost every race and language you can think of under the sun, hence the common moniker, “the melting pot of the world.” The city is made up of 5 boroughs: Manhattan, Brooklyn, Queens, the Bronx and Staten Island, all of which were “grouped” together into a single city in 1898."
+        description:"New York City is the most diverse and populated city in the United States (the city has a population density of over 27,000 people per square mile). In New York, you’ll find almost every race and language you can think of under the sun, hence the common moniker, “the melting pot of the world.” The city is made up of 5 boroughs: Manhattan, Brooklyn, Queens, the Bronx and Staten Island, all of which were “grouped” together into a single city in 1898. The city's diversity and linguistic variety create opportunities to learn about different cultures and interact with friendly locals. New York City is known for its iconic Broadway shows, stunning views from the High Line, world-class museums like MoMA, and its reputation as a fashion capital."
     },
     {
         id:3,
         name:"COLOSSEUM",
         img:"colosseum-Italy.png",
         location:"https://maps.app.goo.gl/e6rYiuf3sHu7apsRA",
-        description:""
+        description:"The Colosseum is built of travertine limestone, tuff (volcanic rock), and brick-faced concrete. It could hold an estimated 50,000 to 80,000 spectators at various points in its history, having an average audience of some 65,000 it was used for gladiatorial contests and public spectacles including animal hunts, executions, re-enactments of famous battles, and dramas based on Roman mythology, and briefly mock sea battles. The building ceased to be used for entertainment in the early medieval era. It was later reused for such purposes as housing, workshops, quarters for a religious order, a fortress, a quarry, and a Christian shrine.Although substantially ruined by earthquakes and stone robbers taking spolia, the Colosseum is still a renowned symbol of Imperial Rome and was listed as one of the New 7 Wonders of the World. It is one of Rome's most popular tourist attractions and has links to the Catholic Church, as each Good Friday the Pope leads a torchlit Way of the Cross procession that starts in the area around the Colosseum. The Colosseum is depicted on the Italian version of the 5 euro cent coin."
     },
     {
         id:4,
-        name:"GOKARNA BEACH",
+        name:"GOKARNA ",
         img:"gokarna-beach.jpg",
         location:"https://maps.app.goo.gl/QmtNxCkAHAuNdkrL7",
-        description:""
+        description:"Gokarna is a small temple town located in the Uttara Kannada district of Karnataka state in southern India, It has a population of around 20,000. Shiva is the most worshipped deity in the town. Gokarna's main temple, Mahabaleshwara, is dedicated to Shiva. The temple houses what is believed by some to be the earliest Shiva lingam (Atmalinga). Gokarna is known as one of seven important centers of pilgrimage in Hinduism. It lies on what was once an unspoiled coastline at the estuary of the River Aghanashini. Due to an increase in tourism, the character of the town has changed, and is no longer just a center of pilgrimage, though large numbers of Shiva devotees continue to visit for prayer and worship. Gokarna is the one of the best beach destinations for tourists in the whole of India. It is very popular for beach trekkers."
     },
     {
         id:5,
         name:"LEH LADAKH",
         img:"leh-ladakh.jpg",
         location:"https://maps.app.goo.gl/8C2zkovv5QoZFJhN9",
-        description:""
+        description:"Leh is a city in the Indian Union territory of Ladakh. It is the largest city and the joint capital of Ladakh. Leh, located in the Leh district, was also the historical capital of the Kingdom of Ladakh. The seat of the kingdom, Leh Palace, the former residence of the royal family of Ladakh, was built in the same style and about the same time as the Potala Palace in Tibet. Since they were both constructed in a similar style and at roughly the same time, the Potala Palace in Tibet and Leh Palace, the royal residence, are frequently contrasted. Leh is at an altitude of 3,524 m (11,562 ft), and is connected via National Highway 1 to Srinagar in the southwest and to Manali in the south via the Leh-Manali Highway (part of National Highway 3)."
     },
     {
         id:6,
         name:"SISSU VALLEY",
         img:"Sissu-Valley.jpg",
         location:"https://maps.app.goo.gl/uB26usxsyLFTbkMs6",
-        description:""
+        description:"Sissu (also known as Khwaling) is a small town in the Lahaul valley of Himachal Pradesh in India. It is around 40 km from Manali and located on the right bank of Chandra river. It is located at 32°29′0″N 77°7′0″E.[1] It is 3,120 m (10,235 ft) above sea level. It is 14 km (9 miles) from Koksar to the south-east and 11 km. (7 miles) west to Gondhla. Sissu is famous due to its representation of the rural way of life. Sissu is untouched by the concepts of urbanization and reflects the truest picture of nature. The gorgeous glaciers, snow-peaked Himalayan ranges, beautiful willow, and poplar trees, etc., contribute to making Sissu attractive."
     },
     {
         id:7,
         name:"SONMARG",
         img:"sonmarg.jpg",
         location:"https://maps.app.goo.gl/WZeBf92Nqnww87dH6",
-        description:""
+        description:"Located in the famous Jammu & Kashmir region, Sonmarg is renowned for its breathtaking landscapes, stunning natural beauty and picturesque meadows. Moreover, the place also serves as the gateway to several hiking trails such as the Amarnath Cave and Thajiwas Glacier. Sonamarg had historical significance as a gateway on the ancient Silk Road, connecting Kashmir with Tibet. Today, the hill station is a popular tourist destination amongst fishers and hikers, and following the Kargil War with neighbouring Pakistan in 1999, serves as a strategically important point for the Indian Army."
     },
     {
         id:8,
         name:"TAJ MAHAL",
         img:"taj-mahal.png",
         location:"https://maps.app.goo.gl/GDhkVHM36NEuSCJGA",
-        description:""
+        description:"The Taj Mahal was commissioned by Shah Jahan in 1631, to be built in the memory of his wife Mumtaz Mahal, who died on 17 June that year, while giving birth to their 14th child, Gauhara Begum. Construction started in 1632, and the mausoleum was completed in 1648, while the surrounding buildings and garden were finished five years later. The imperial court documenting Shah Jahan's grief after the death of Mumtaz Mahal illustrates the love story held as the inspiration for the Taj Mahal. According to contemporary historians Muhammad Amin Qazvini, Abdul Hamid Lahori and Muhammad Saleh Kamboh, he did not show the same level of affection to others as he had shown her while she was alive. He avoided royal affairs for a week due to his grief, along with giving up listening to music and dressing lavishly for two years. Shah Jahan was enamored by the beauty of the land at the south side of Agra on which a mansion belonging to Raja Jai Singh I stood. This place was chosen for the construction of Mumtaz's tomb by Shah Jahan and Jai Singh agreed to donate it to the emperor."
     },
     {
         id:9,
         name:"BLUE MOSQUE",
         img:"Blue-Mosque-Turkey.png",
         location:"https://maps.app.goo.gl/VQfNus9go4Fm9fBD7",
-        description:""
+        description:"The Blue Mosque in Istanbul, also known by its official name, the Sultan Ahmed Mosque , is an Ottoman-era historical imperial mosque located in Istanbul, Turkey. It was constructed between 1609 and 1617 during the rule of Ahmed 1 and remains a functioning mosque today. It also attracts a large number of tourists and is one of the most iconic and popular monuments of Ottoman architecture.The mosque has a classical Ottoman layout with a central dome surrounded by four semi-domes over the prayer hall. It is fronted by a large courtyard and flanked by six minarets. On the inside, it is decorated with thousands of Iznik tiles and painted floral motifs in predominantly blue colours, which give the mosque its popular name. The mosque was built next to the former Hippodrome and stands across from the Hagia Sophia, another popular tourist site."
     },
     {
         id:10,
         name:"SWITZERLAND",
         img:"switzerland.jfif",
         location:"https://maps.app.goo.gl/ZvNtbWiq7mqNq2z59",
-        description:""
+        description:"Switzerland is a landlocked country in Central Europe. Neighboring countries include Austria, France, Italy, Liechtenstein, and Germany. The geography of Switzerland is mostly mountains (Alps in the south, Jura in the northwest) with a central plateau of rolling hills, plains, and large lakes. Switzerland is a small mountainous country located in central Europe. This landlocked country is about the size of New Jersey and is between France and Italy. It is also bordered by Austria, Germany, and Liechtenstein.Most of the population lives in the plateau which is between the high Alps in the south and the Jura mountains in the north. The mountainous area in the south is sparsely populated."
     },
-
-
 ]
