1c64a6f8e5cb95193a86ae3097b1ee6d2dd5f8d3
Shashwat Bhardwaj <shashwatbhardwaj3@gmail.com>
2024-04-18 14:45:48 +0530

    added all the location in the data component

--- src/components/Data.js
+++ src/components/Data.js
@@ -3,70 +3,70 @@ export default [
         id:1,
         name:"GREAT WALL OF CHINA",
         img:"Great_Wall_of_China.jpg",
-        location:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3805.9023414717044!2d78.28465537501835!3d17.464387700582613!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcbeda74ba515f7%3A0x6cef6ce3c6e9a0c5!2sThe%20Great%20Wall!5e0!3m2!1sen!2sin!4v1713374963901!5m2!1sen!2sin" ,
-        description:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae ex illum, ratione nemo voluptatum tenetur quas. Earum aperiam fugit iure sapiente. Aliquam, beatae! Non, placeat, accusantium saepe ut possimus cumque fugiat molestias molestiae vel vitae ad quaerat obcaecati! Facere neque accusamus incidunt fugit quia totam libero adipisci inventore dolor, consequatur laboriosam animi dolore soluta, ipsam corporis, eveniet quasi blanditiis nulla possimus quibusdam culpa nobis minus perspiciatis aut! Ducimus, accusantium fugit? Corporis debitis enim magnam dolor assumenda iusto. Labore, corporis iusto delectus cumque dolorem debitis, quisquam possimus excepturi aspernatur accusantium id ex sit sint doloribus nulla sunt, voluptatum unde quae. Aperiam."
+        location:"https://maps.app.goo.gl/F6obeJ7AQvp4Ufxc6",
+        description:"The Great Wall of China is an ancient wall in China. The wall is made of cement, rocks, bricks, and dirt. It was finished in 1878 and it was meant to protect the north of the empire of China from enemy attacks. It is the longest structure humans have ever built. It is about 21,196 kilometers long, 9.1 metres (30 feet) wide and 20 metres high. The earlier sections on the wall are made of compacted dirt and stone. Later in the Ming Dynasty they used bricks. There are 7,000 watch towers, block houses for soldiers and beacons to send smoke signals.Nineteen walls have been built that were called the Great Wall of China. The first was built in the 7th century BC. The most famous wall was built between 226 and 200 BC by the first Emperor of China, Qin Shi Huang (Qin Pronounced as Chin), during the Qin Dynasty. Not much of this wall remains as people have been stealing from it. It was much farther north than the current wall. The current wall was built during the Ming Dynasty."
     },
     {
         id:2,
         name:"NEW YORK",
         img:"new-york.jpeg",
-        location:"",
-        description:""
+        location:"https://maps.app.goo.gl/6yunkVspWXMSuxsq6",
+        description:"New York City is the most diverse and populated city in the United States (the city has a population density of over 27,000 people per square mile). In New York, you’ll find almost every race and language you can think of under the sun, hence the common moniker, “the melting pot of the world.” The city is made up of 5 boroughs: Manhattan, Brooklyn, Queens, the Bronx and Staten Island, all of which were “grouped” together into a single city in 1898."
     },
     {
         id:3,
         name:"COLOSSEUM",
         img:"colosseum-Italy.png",
-        location:"",
+        location:"https://maps.app.goo.gl/e6rYiuf3sHu7apsRA",
         description:""
     },
     {
         id:4,
         name:"GOKARNA BEACH",
         img:"gokarna-beach.jpg",
-        location:"",
+        location:"https://maps.app.goo.gl/QmtNxCkAHAuNdkrL7",
         description:""
     },
     {
         id:5,
         name:"LEH LADAKH",
         img:"leh-ladakh.jpg",
-        location:"",
+        location:"https://maps.app.goo.gl/8C2zkovv5QoZFJhN9",
         description:""
     },
     {
         id:6,
         name:"SISSU VALLEY",
         img:"Sissu-Valley.jpg",
-        location:"",
+        location:"https://maps.app.goo.gl/uB26usxsyLFTbkMs6",
         description:""
     },
     {
         id:7,
         name:"SONMARG",
         img:"sonmarg.jpg",
-        location:"",
+        location:"https://maps.app.goo.gl/WZeBf92Nqnww87dH6",
         description:""
     },
     {
         id:8,
         name:"TAJ MAHAL",
         img:"taj-mahal.png",
-        location:"",
+        location:"https://maps.app.goo.gl/GDhkVHM36NEuSCJGA",
         description:""
     },
     {
         id:9,
         name:"BLUE MOSQUE",
         img:"Blue-Mosque-Turkey.png",
-        location:"",
+        location:"https://maps.app.goo.gl/VQfNus9go4Fm9fBD7",
         description:""
     },
     {
         id:10,
         name:"SWITZERLAND",
         img:"switzerland.jfif",
-        location:"",
+        location:"https://maps.app.goo.gl/ZvNtbWiq7mqNq2z59",
         description:""
     },
 
